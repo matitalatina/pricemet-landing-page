12153d452f51b3b787c3c9bc19668f6c4259810d
Mattia Natali <matitalatina@gmail.com>
2020-06-14 14:16:15 +0200

    Added app store link

--- site-config.js
+++ site-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // App Info
-  appstore_link         : null,                                                   // Enter App Store URL.
-  playstore_link        : "https://play.google.com/store/apps/details?id=it.mattianatali.gaslowapp&hl=it",                                  // Enter Google Play Store URL.
+  appstore_link         : "https://apps.apple.com/it/app/metext-estrai-testo-ocr/id1510120975",               // Enter App Store URL.
+  playstore_link        : "https://play.google.com/store/apps/details?id=it.mattianatali.gaslowapp&hl=it",    // Enter Google Play Store URL.
   google_analytics_ID   : "UA-141237367-1",                                       // Enter Google Analytics ID or ""
   presskit_download_link: null,                                                   // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file). 
   video_or_screenshot   : "screenshot",                                           // "screenshot" or "video"
