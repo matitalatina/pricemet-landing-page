baf62869d08a65cfda4ae67882eea93fabeabfc4
Mattia Natali <mattia.natali@klarna.com>
2022-10-06 22:57:28 +0200

    Added appstore link

--- site-config.js
+++ site-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // App Info
-  // appstore_link         : "https://apps.apple.com/it/app/gaslow-prezzo-carburanti/id1510630580",              // Enter App Store URL.
+  appstore_link         : "https://apps.apple.com/it/app/pricemet-notifica-prezzi/id6443516529",        // Enter App Store URL.
   playstore_link        : "https://play.google.com/store/apps/details?id=com.mattianatali.pricemet",    // Enter Google Play Store URL.
   google_analytics_ID   : "UA-141237367-1",                                       // Enter Google Analytics ID or ""
   presskit_download_link: null,                                                   // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file). 
